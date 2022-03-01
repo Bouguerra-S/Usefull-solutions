7c7dc987193ff43102ac62b6ccc44330219e2f6c
Seifeddine Bouguerra <seif2@hotmail.fr>
2022-03-01 18:14:24 +0100

    partial bulk add

---
+++ AT-NetCore-NotepadPlusPlus/[Windows Form Application]/AT-Notepad.WFA.NetCore.Infrastructure/First_Style/CONST.cs
@@ -0,0 +1,7 @@
+﻿namespace AT_Notepad.WFA.NetCore.Infrastructure.First_Style
+{
+    public static class CONST
+    {
+        public const string CannotFindMessage = "Cannot find \"{SearchText}\"";
+    }
+}
