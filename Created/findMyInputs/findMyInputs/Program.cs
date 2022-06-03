2fb048aa8839c6843a04eb49cf92eedeb64e8155
Bouguerra-S <seifeddine.bouguerra@cf.group>
2022-06-03 21:19:17 +0200

    ap wpd

---
+++ Created/findMyInputs/findMyInputs/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Management;
+
+namespace findMyInputs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManagementObjectSearcher objSearcher = new ManagementObjectSearcher(
+           "SELECT * FROM Win32_Keyboard");
+
+            ManagementObjectCollection objCollection = objSearcher.Get();
+
+            foreach (ManagementObject obj in objCollection)
+            {
+                foreach (PropertyData property in obj.Properties)
+                {
+                    if (property.Name=="Description" || property.Name=="Status")
+                    {
+                        Console.Out.WriteLine(String.Format("{0}:{1}", property.Name, property.Value));
+
+                    }
+                }
+            }
+
+            objSearcher = new ManagementObjectSearcher(
+           "SELECT * FROM Win32_PointingDevice");
+
+            objCollection = objSearcher.Get();
+
+            foreach (ManagementObject obj in objCollection)
+            {
+                foreach (PropertyData property in obj.Properties)
+                {
+                    if (property.Name == "Description" || property.Name == "Status")
+                    {
+                        Console.Out.WriteLine(String.Format("{0}:{1}", property.Name, property.Value));
+
+                    }
+                }
+            }
+            //todo: cameras microphones scanners
+        }
+    }
+}
