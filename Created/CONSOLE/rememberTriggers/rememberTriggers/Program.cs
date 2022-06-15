549d1c00088e6606fe089b28c1eccd7c52b8219a
Bouguerra-S <36759537+Bouguerra-S@users.noreply.github.com>
2022-06-15 20:17:12 +0200

    triggers

---
+++ Created/CONSOLE/rememberTriggers/rememberTriggers/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, do you remember your triggers? yes/no");
+string reply = Console.ReadLine();
+switch (reply)
+{
+    case "yes":
+
+    default:
+        break;
+}
