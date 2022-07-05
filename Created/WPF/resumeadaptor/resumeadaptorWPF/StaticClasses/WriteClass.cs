af28651dd39753ccd6a8ea54d03c93547cfdcc95
Bouguerra-S <36759537+Bouguerra-S@users.noreply.github.com>
2022-07-05 11:00:59 +0200

    DEV

--- Created/WPF/resumeadaptor/resumeadaptorWPF/StaticClasses/WriteClass.cs
+++ Created/WPF/resumeadaptor/resumeadaptorWPF/StaticClasses/WriteClass.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace resumeadaptorWPF.StaticClasses
 {
@@ -11,7 +13,11 @@ namespace resumeadaptorWPF.StaticClasses
     {
         public  async Task WriteString(string mystring)
         {
-            await File.WriteAllTextAsync(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\myCV.txt", mystring);
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            if (saveFileDialog1.ShowDialog() == true)
+            {
+                await File.WriteAllTextAsync(saveFileDialog1.FileName , mystring);
+            }
         }
 
     }
---
+++ Created/WPF/resumeadaptor/resumeadaptorWPFTests/ViewModels/CVViewModelTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using resumeadaptorWPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace resumeadaptorWPF.ViewModels.Tests
+{
+    [TestClass()]
+    public class CVViewModelTests
+    {
+        [TestMethod()]
+        public void CVViewModelTest()
+        {
+            CVViewModel testcvvm = new CVViewModel();
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void AddSectionTest()
+        {
+            CVViewModel testcvvm = new CVViewModel();
+            testcvvm.AddSection();
+            Assert.IsTrue(true);
+        }
+    }
+}
