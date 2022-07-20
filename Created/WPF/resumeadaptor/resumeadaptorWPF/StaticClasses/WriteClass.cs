5360b226aee8ad635ac9f245694605d4884890bc
Bouguerra-S <seifeddine.bouguerra@cf.group>
2022-07-20 20:30:27 +0200

    Update WriteClass.cs

--- Created/WPF/resumeadaptor/resumeadaptorWPF/StaticClasses/WriteClass.cs
+++ Created/WPF/resumeadaptor/resumeadaptorWPF/StaticClasses/WriteClass.cs
@@ -1,23 +1,16 @@
 ﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 
 namespace resumeadaptorWPF.StaticClasses
 {
     public class WriteClass
     {
-        public  async Task WriteString(string mystring)
+        public async Task WriteString(string myString)
         {
             SaveFileDialog saveFileDialog1 = new SaveFileDialog();
             if (saveFileDialog1.ShowDialog() == true)
-            {
-                await File.WriteAllTextAsync(saveFileDialog1.FileName , mystring);
-            }
+                await File.WriteAllTextAsync(saveFileDialog1.FileName, myString);
         }
 
     }
